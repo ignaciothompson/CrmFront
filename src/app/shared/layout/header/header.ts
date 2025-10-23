749fbbb8a382654acfd04775a00a303b0ccafe29
Ignacio Thompson <chonathomp@gmail.com>
2025-10-22 21:51:26 -0300

    Mejoras en login y permisos

--- src/app/shared/layout/header/header.ts
+++ src/app/shared/layout/header/header.ts
@@ -1,11 +1,14 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
+import { Auth } from '@angular/fire/auth';
+import { signOut } from 'firebase/auth';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NgbDropdownModule],
   templateUrl: './header.html',
   styleUrl: './header.css'
 })
@@ -13,10 +16,17 @@ export class Header {
 	@Input() title: string = '';
 	@Input() userName: string = '';
 
-	constructor(private router: Router) {}
+	constructor(private router: Router, private auth: Auth) {}
 
 	goToUserSettings() {
 		this.router.navigateByUrl('/usuario');
+	}
+
+	async logout() {
+		try {
+			await signOut(this.auth);
+			this.router.navigateByUrl('/login');
+		} catch {}
 	}
 
   goHome() {
